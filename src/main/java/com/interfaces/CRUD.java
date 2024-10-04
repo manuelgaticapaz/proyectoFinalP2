25a5f3764647bb7937618dfdc48eb194d746e080
manuelgaticapaz <manuel.gatica.paz.94@gmail.com>
2024-10-04 16:18:50 -0600

    Inteface nueva

---
+++ src/main/java/com/interfaces/CRUD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.interfaces;
+
+import com.modelo.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Kenzy
+ */
+public class CRUD {
+    /*public List listar();
+    public Usuario list(int id);
+    public boolean add(Usuario per);
+    public boolean edit(Usuario per);
+    public boolean eliminar(int id)*/
+}
